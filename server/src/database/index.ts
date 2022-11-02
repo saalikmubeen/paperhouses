4a6a605157c5421a49b73c4c2b2fe718efbd0ea9
saalik123 <63722568+saalik123@users.noreply.github.com>
2022-11-02 12:48:35 +0530

    changed mongo uri

--- server/src/database/index.ts
+++ server/src/database/index.ts
@@ -1,13 +1,18 @@
 import { MongoClient } from "mongodb";
 import { Booking, Chat, Database, Listing, Message, User } from "../lib/types";
 
+const username = encodeURIComponent(process.env.MONGO_USER!);
+const password = encodeURIComponent(process.env.MONGO_PASSWORD!);
+
+let uri = `mongodb+srv://${username}:${password}@paper-houses.3df2fiq.mongodb.net/?retryWrites=true&w=majority`;
+
 const url =
     process.env.NODE_ENV === "production"
-        ? process.env.MONGO_URI
+        ? uri
         : "mongodb://localhost/paper-houses";
 
 export const connectDatabase = async (): Promise<Database> => {
-    const client = await MongoClient.connect(url!, {
+    const client = await MongoClient.connect(url, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
     });
@@ -15,9 +20,9 @@ export const connectDatabase = async (): Promise<Database> => {
 
     return {
         bookings: db.collection<Booking>("bookings"),
-        listings: db.collection<Listing>("listings"),
         users: db.collection<User>("users"),
         messages: db.collection<Message>("messages"),
         chat: db.collection<Chat>("chat"),
+        listings: db.collection<Listing>("listings"),
     };
 };
